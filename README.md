cc4152581e8509741dd4933905c659a5ca0cf1ff
Erick Willian <134653806+EriickWill@users.noreply.github.com>
2023-12-26 18:11:22 -0300

    .

---
+++ README.md
@@ -0,0 +1,5 @@
+# Weather App 
+
+- App de previsão de clima onde você pode procurar pelo país e ele mostra a temperatura atual do local escolhido 
+
+<img src="./public/img.png"/>
---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+  const key = "b45b7aa3cd526e81a9d5d7ee9bb76f86";
+  const [weathers, setWeathers] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [city, setCity] = useState("São Paulo");
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const params = {
+      q: city,
+      appid: key,
+      units: "metric",
+    };
+    axios.get(baseUrl, { params }).then((response) => {
+      console.log(response.data);
+      setWeathers(response.data);
+      setLoading(false)
+    });
+
+    console.log(weathers);
+  }, [city]);
+
+  function searchTemp(e) {
+    e.preventDefault();
+    setLoading(false)
+    setCity(ref.current.value);
+    console.log(city);
+  }
+
+  return (
+    <main>
+      <section className='container-clima'>
+        <form>
+          <input type="text" ref={ref} />
+          <button onClick={searchTemp}>Search</button>
+        </form>
+ 
+        {loading == true ? (
+            <div>Carregando</div>
+          ) : (
+            <div className='weather-container'>
+              <h1>{weathers.name}</h1>
+              <span>{Math.floor(weathers.main.temp)}<p>°</p></span>
+              <p>{weathers.weather[0].main}</p>
+            </div>
+          )}
+
+       
+      </section>
+    </main>
+  );
+}
+
+export default App;
